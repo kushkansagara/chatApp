095586fa48cc464c90c8fd4d4c73b2f3ec6b3aba
kushkansagara <110079442+kushkansagara@users.noreply.github.com>
2023-10-15 11:47:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const server = require("http").createServer(app);
 const PORT = 5001;
 const io = require("socket.io")(server, {
   cors: {
-    origin: "chat-app-ten-henna.vercel.app",
+    origin: "https://chat-app-ten-henna.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
